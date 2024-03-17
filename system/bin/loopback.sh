bf2ba99e0fe620270b30bb1b5aa3c7b1a137e8bf
ZH-XiJun <root@asdfghjkl-Win11.localdomain>
2024-03-17 17:28:02 +0800

    Add system for sw766-user 8.1.0 OPM2.171019.012_T24A0308 354
    Signed-off-by: ZH-XiJun <root@asdfghjkl-Win11.localdomain>

---
+++ system/bin/loopback.sh
@@ -0,0 +1,24 @@
+#! /system/bin/sh
+echo "loopback test"
+spk=1
+rec=2
+mic1=3
+mic2=4
+enable=1
+disable=0
+open="-Y"
+close="-N"
+
+if test $2 -eq $enable
+then
+	loopbacktest $open "$1"
+elif test $2 -eq $disable
+then
+	loopbacktest $close "$1"
+	setprop persist.sys.loopback-spk 2
+	setprop persist.sys.loopback-rec 2
+	setprop persist.sys.loopback-mic1 2
+	setprop persist.sys.loopback-mic2 2
+else
+	echo "input error ctl cmd!"
+fi
